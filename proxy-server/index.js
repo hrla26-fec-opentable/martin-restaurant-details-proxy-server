91b99261ec6c657df4c3d1c753969ca8425b2c47
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2019-01-11 14:26:48 -0800

    Run proxy server locally

--- proxy-server/index.js
+++ proxy-server/index.js
@@ -4,48 +4,56 @@ const proxy = require('./proxy.js');
 const PORT = 9000;
 
 proxy.get('/api/reviews/all/:restaurantId', (req, res) => {
+  console.log('proxy get reviews all');
   const { restaurantId } = req.params;
-  axios.get(`http://3.86.82.182:9001/api/reviews/all/${restaurantId}`)
-    .then((data) => {
+  axios.get(`http://127.0.0.1:9001/api/reviews/all/${restaurantId}`)
+  // axios.get(`http://3.86.82.182:9001/api/reviews/all/${restaurantId}`)
+    .then(({data}) => {
       res.send(data);
     });
 });
 
 proxy.get('/api/reviews/summary/:restaurantId', (req, res) => {
   const { restaurantId } = req.params;
-  axios.get(`http://3.86.82.182:9001/api/reviews/summary/${restaurantId}`)
-    .then((data) => {
+  axios.get(`http://127.0.0.1:9001/api/reviews/summary/${restaurantId}`)
+  // axios.get(`http://3.86.82.182:9001/api/reviews/summary/${restaurantId}`)
+    .then(({data}) => {
       res.send(data);
     });
 });
 
 proxy.get('/api/nav/:metro', (req, res) => {
   const { metro } = req.params;
-  axios.get(`http://3.86.82.182:9004/api/nav/${metro}`)
-    .then((data) => {
+  axios.get(`http://127.0.0.1:9004/api/nav/${metro}`)
+  // axios.get(`http://3.86.82.182:9004/api/nav/${metro}`)
+    .then(({data}) => {
       res.send(data);
     });
 });
 
 proxy.get('/api/search/:searched', (req, res) => {
   const { searched } = req.params;
-  axios.get(`http://3.86.82.182:9004/api/nav/${searched}`)
-    .then((data) => {
+  axios.get(`http://127.0.0.1:9004/api/search/${searched}`)
+  // axios.get(`http://3.86.82.182:9004/api/nav/${searched}`)
+    .then(({data}) => {
       res.send(data);
     });
 });
 
 proxy.get('/api/menus/:rest_id', (req, res) => {
+  console.log('proxy menu get request');
   const { rest_id } = req.params;
-  axios.get(`http://3.86.82.182:9003/api/menus/${rest_id}`)
-    .then((data) => {
+  axios.get(`http://127.0.0.1:9003/api/menus/${rest_id}`)
+  // axios.get(`http://3.86.82.182:9003/api/menus/${rest_id}`)
+    .then(({data}) => {
       res.send(data);
     });
 });
 
 proxy.get('/api/restaurant', (req, res) => {
-  axios.get(`http://3.86.82.182:9002/api/restaurant`, {params: {id: 2}})
-    .then((data) => {
+  axios.get(`http://127.0.0.1:9002/api/restaurant`, {params: {id: 2}})
+  // axios.get(`http://3.86.82.182:9002/api/restaurant`, {params: {id: 2}})
+    .then(({data}) => {
       res.send(data);
     });
 });
--- proxy-server/proxy.js
+++ proxy-server/proxy.js
@@ -2,11 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const proxy = express();
 
+proxy.use(bodyParser.json());
+proxy.use(bodyParser.urlencoded({ extended: true }));
 proxy.use(cors());
 proxy.use(morgan('dev'));
-proxy.use(express.static(path.join(__dirname, 'public')));
+proxy.use(express.static(path.join(__dirname, '../public')));
 
 module.exports = proxy;
