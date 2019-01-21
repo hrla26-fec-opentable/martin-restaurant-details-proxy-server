9de59cbd78e4cf42bdac42cdc222860e213676c0
Martin Han <martinhan@martins-mbp-2.attlocal.net>
2019-01-20 17:40:12 -0800

    Remove dev comments, remove morgan middleware

--- proxy-server/proxy.js
+++ proxy-server/proxy.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -9,7 +8,6 @@ const proxy = express();
 proxy.use(bodyParser.json());
 proxy.use(bodyParser.urlencoded({ extended: true }));
 proxy.use(cors());
-proxy.use(morgan('dev'));
 proxy.use(express.static(path.join(__dirname, '../public')));
 
 module.exports = proxy;
