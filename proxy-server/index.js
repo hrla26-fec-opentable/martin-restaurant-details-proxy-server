6272c49fdb62c3cacaf014b1435a8ff666e3a772
Martin Han <martinhan@martins-mbp-2.attlocal.net>
2019-03-14 14:40:24 -0700

    Use ec2 instance

--- proxy-server/index.js
+++ proxy-server/index.js
@@ -5,41 +5,41 @@ const PORT = 9000;
 
 proxy.get('/api/reviews/all/:restaurantId', (req, res) => {
   const { restaurantId } = req.params;
-  axios.get(`http://localhost:9001/api/reviews/all/${restaurantId}`)
+  axios.get(`http://100.24.12.73:9001/api/reviews/all/${restaurantId}`)
     .then(({ data }) => { res.send(data); })
     .catch((err) => { console.error(err); });
 });
 
 proxy.get('/api/reviews/summary/:restaurantId', (req, res) => {
   const { restaurantId } = req.params;
-  axios.get(`http://localhost:9001/api/reviews/summary/${restaurantId}`)
+  axios.get(`http://100.24.12.73:9001/api/reviews/summary/${restaurantId}`)
     .then(({ data }) => { res.send(data); })
     .catch((err) => { console.error(err); });
 });
 
 proxy.get('/api/nav/:metro', (req, res) => {
   const { metro } = req.params;
-  axios.get(`http://localhost:9004/api/nav/${metro}`)
+  axios.get(`http://100.24.12.73:9004/api/nav/${metro}`)
     .then(({ data }) => { res.send(data); })
     .catch((err) => { console.error(err); });
 });
 
 proxy.get('/api/search/:searched', (req, res) => {
   const { searched } = req.params;
-  axios.get(`http://localhost:9004/api/search/${searched}`)
+  axios.get(`http://100.24.12.73:9004/api/search/${searched}`)
     .then(({ data }) => { res.send(data); })
     .catch((err) => { console.error(err); });
 });
 
 proxy.get('/api/menus/:rest_id', (req, res) => {
   const { rest_id } = req.params;
-  axios.get(`http://localhost:9003/api/menus/${rest_id}`)
+  axios.get(`http://100.24.12.73:9003/api/menus/${rest_id}`)
     .then(({ data }) => { res.send(data); })
     .catch((err) => { console.error(err); });
 });
 
 proxy.get('/api/restaurant', (req, res) => {
-  axios.get(`http://localhost:9002/api/restaurant`, { params: { id: 2 } })
+  axios.get(`http://100.24.12.73:9002/api/restaurant`, { params: { id: 2 } })
     .then(({ data }) => { res.send(data); })
     .catch((err) => { console.error(err); });
 });
